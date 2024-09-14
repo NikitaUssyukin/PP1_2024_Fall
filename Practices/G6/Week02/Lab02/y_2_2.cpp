6029f0da6224a93c0c2b266fe3545624307d6b93
NikitaUssyukin <nikus0108@gmail.com>
2024-09-14 09:43:13 +0500

    update

---
+++ Practices/G6/Week02/Lab02/y_2_2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a; // day
+    int b; // month
+    int c; // year
+
+    cin >> a >> b >> c;
+
+    bool validDate = false;
+
+    if((1 <= a && a <= 31) && (1 <= b && b <= 12)) {
+        if(b == 2 && a == 29) {
+            if((c % 4 == 0 && c % 100 != 0) || (c % 400 == 0)) {
+                validDate = true;
+            }
+        }
+        else if(b == 2 && a <= 28) {
+            validDate = true;
+        }
+        else if(a == 31) {
+            if(b == 1 || b == 3 || b == 5 || b == 7 || b == 8 || b == 10 || b == 12) {
+                validDate = true;
+            }
+        } 
+        else {
+            validDate = true;
+        }
+    }
+
+    if(validDate) {
+        cout << "yes\n";
+    }
+    else {
+        cout << "no\n";
+    }
+
+    return 0;
+}
