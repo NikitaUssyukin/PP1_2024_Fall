ad48fcd5a97a8a44bf642b0c3896aee02243e84e
NikitaUssyukin <nikus0108@gmail.com>
2024-10-10 00:46:25 +0500

    update

--- Lectures/G1/Week06/string/n.cpp
+++ Lectures/G1/Week06/string/n.cpp
@@ -6,8 +6,9 @@ int main() {
 
     string s = "Big cat carries a carpet";
 
-    cout << s.find("cat") << endl;
-    //cout << s.find("car") << endl;
+    cout << s.find("cat") << endl; // Output: 4
+    cout << s.find("car") << endl; // Output: 8
+    cout << s.find("dog") << endl; // Output: 18446744073709551615 (string::npos)
     
     return 0;
 }
