c364200bf5a82e770078faffb2f6585743ea4c43
NikitaUssyukin <nikus0108@gmail.com>
2024-11-08 15:37:05 +0500

    update

---
+++ Practices/G5/Week10/b.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    deque<int> dq;
+
+    while(n > 0) {
+        dq.push_front(n);
+        int reps = n % dq.size();
+        while(reps > 0) {
+            dq.push_front(dq.back());
+            dq.pop_back();
+            --reps;
+        }
+        --n;
+    }
+
+    for(int i = 0; i < dq.size(); ++i) {
+        cout << dq[i] << ' ';
+    }
+    cout << endl;
+
+    
+    return 0;
+}
