675621b441edf963f55b6ed182e7aa3dad577b66
NikitaUssyukin <nikus0108@gmail.com>
2024-11-21 17:07:14 +0500

    update

--- Practices/G3/Week10/b.cpp
+++ Practices/G3/Week10/b.cpp
@@ -13,14 +13,14 @@ int main() {
 
     set<char> s(str.begin(), str.end());
 
-    // set<char>::iterator it1;
+    set<char>::iterator it1;
 
     for(set<char>::iterator it1 = s.begin(); it1 != s.end(); ++it1) {
         ++m[*it1];
     }
 
-    // map<char, int>::iterator it2;
-    for(map<char, int>::iterator it2 = m.begin(); it2 != m.end(); ++it2) {
+    map<char, int>::iterator it2;
+    for(it2 = m.begin(); it2 != m.end(); ++it2) {
         cout << it2->first << ' ' << it2->second << endl; 
     }
 
