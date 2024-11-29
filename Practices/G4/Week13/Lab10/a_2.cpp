4288d43daf62896040cf35c08441d96ec48dcbc2
NikitaUssyukin <nikus0108@gmail.com>
2024-11-29 14:18:33 +0500

    update

---
+++ Practices/G4/Week13/Lab10/a_2.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct point {
+    int x, y;
+    double distance;
+
+    void calcDistance(int p_x, int p_y) {
+        distance = sqrt(pow(p_x - x, 2) + pow(p_y - y, 2));
+    }
+
+    void print() {
+        cout << x << ' ' << y << endl;
+    }
+
+    bool operator < (point another) {
+        return this->distance < another.distance;
+    }
+};
+
+
+
+int main() {
+
+    point p;
+    cin >> p.x >> p.y;
+
+    int n;
+    cin >> n;
+
+    vector<point> v;
+
+    while(n > 0) {
+
+        point a;
+        cin >> a.x >> a.y;
+
+        a.calcDistance(p.x, p.y);
+
+        v.push_back(a);
+
+        --n;
+    }
+
+    sort(v.begin(), v.end());
+
+    for(int i = 0; i < v.size(); ++i) {
+        v[i].print();
+    }
+
+    return 0;
+}
